16ac7ef4a51adcb1ec6a11339d828e935c6bc854
gaoxin <gaoxin@360.net>
2018-03-15 17:04:50 +0800

    commit

---
+++ js/observer.js
@@ -0,0 +1,59 @@
+function Observer (data) {
+  this.data = data
+  this.walk(data)
+}
+
+Observer.prototype = {
+  walk: function (data) {
+    var self = this
+    Object.keys(data).forEach(function (key) {
+      self.defineReactive(data, key, data[key])
+    })
+  },
+  defineReactive: function (data, key, val) {
+    var dep = new Dep()
+    console.log(1111111, dep)
+    var childObj = observe(val)
+    Object.defineProperty(data, key, {
+      enumerable: true,
+      configurable: true,
+      get: function getter () {
+        if (Dep.target) {
+          dep.addSub(Dep.target)
+        }
+        return val
+      },
+      set: function setter (newVal) {
+        console.log(222222, dep)
+        if (newVal === val) {
+          return
+        }
+        val = newVal
+        dep.notify()
+      },
+    })
+  },
+}
+
+function observe (value, vm) {
+  if (!value || typeof value !== 'object') {
+    return
+  }
+  return new Observer(value)
+}
+
+function Dep () {
+  this.subs = []
+}
+
+Dep.prototype = {
+  addSub: function (sub) {
+    this.subs.push(sub)
+  },
+  notify: function () {
+    this.subs.forEach(function (sub) {
+      sub.update()
+    })
+  },
+}
+Dep.target = null
